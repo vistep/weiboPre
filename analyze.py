5555eed7f4c4a3e3f346c4020fac4b02409a4cb5
vistep <shujiaming@hotmail.com>
2015-08-25 15:28:59 +0800

    2015-8-25 15:30 modify the analyze.py create a file named naiveCluster.py

--- analyze.py
+++ analyze.py
@@ -7,20 +7,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_table(r"weibo_train_data.txt", sep='\t', encoding='utf-8',
+                     header=None, names=['uid', 'mid', 'time', 'forward_count', 'comment_count', 'like_count', 'content'],
                      dtype={'uid': str, 'mid': str, 'time': str, 'forward_count': int,
                             'comment_count': int, 'like_count': int, 'content': str})
 
-print "max forwardCount: " + str(np.max(data['forward_count']))
-print "mean forwardCount " + str(data['forward_count'].mean())
-print "max commentCount: " + str(np.max(data['comment_count']))
-print "mean commentCount " + str(data['comment_count'].mean())
-print "max likeCount: " + str(np.max(data['like_count']))
-print "mean likeCount " + str(data['like_count'].mean())
-plt.figure(1)
-plt.hist(data['forward_count'])
-subdata = data[data.forward_count > 1000]
-print subdata['forward_count'].head()
-print len(subdata['forward_count'])
-plt.figure(2)
-plt.hist(subdata['forward_count'])
-plt.show()
+# print "max forwardCount: " + str(np.max(data['forward_count']))
+# print "mean forwardCount " + str(data['forward_count'].mean())
+# print "max commentCount: " + str(np.max(data['comment_count']))
+# print "mean commentCount " + str(data['comment_count'].mean())
+# print "max likeCount: " + str(np.max(data['like_count']))
+# print "mean likeCount " + str(data['like_count'].mean())
+#
+# forwardArray = data['forward_count'].values
+# commentArray = data['comment_count'].values
+# likeArray = data['like_count'].values
+#
+# plt.figure(1)
+# plt.hist(forwardArray, bins=range(0, 82000, 1000))
+# plt.title("forwardCount")
+# plt.figure(2)
+# plt.hist(commentArray, bins=range(0, 36000, 1000))
+# plt.title('commentCount')
+# plt.figure(3)
+# plt.hist(likeArray, bins=range(0, 8000, 1000))
+# plt.title("likeCount")
+# plt.show()
+#
+# print "forwardAndcomment"
+# print data['forward_count'].corr(data['comment_count'])
+# print "forwardAndlike"
+# print data['forward_count'].corr(data['like_count'])
+# print "commentAndLike"
+# print data['comment_count'].corr(data['like_count'])
+
+userGroup = data.groupby(data.uid)
+
---
+++ naiveCluster.py
@@ -0,0 +1,54 @@
+__author__ = 'Administrator'
+__metaclass__ = type
+import numpy as np
+# -*- coding: utf-8 -*-
+
+class Center:
+    def __init__(self, data, index):
+        self.sampleArr = np.array(data)
+        self.indexLst = []
+        self.indexLst.append(index)
+        self.meanValue = data
+
+    def add(self, data, index):
+        self.sampleArr = np.vstack((self.sampleArr, data))
+        self.indexLst.append(index)
+        self.meanValue = self.sampleArr.mean(axis=0)
+
+    def getNum(self):
+        return len(self.indexLst)
+
+def loadData():
+    # every row contains one sample
+    return np.array([[1, 2, 3], [1, 2, 4], [7, 8, 9]])
+
+def distance(data1, data2):
+    # need to be redefined
+    return np.sum((data1 - data2) ** 2) / np.shape(data1)[0]
+
+def process():
+    limit = 0.5 ### need to be modified
+    dataSet = loadData()
+    print "dataSet"
+    print dataSet
+    centerLst = []
+    centerLst.append(Center(dataSet[0, :], 0))
+    for lines in range(1, np.shape(dataSet)[1]):
+        dis = np.zeros((1, len(centerLst)))
+        for i in range(0, len(centerLst)):
+            print centerLst
+            dis[0, i] = distance(centerLst[i].meanValue, dataSet[lines, :])
+        minDis = dis.min()
+        if minDis < limit:
+            index = np.argmin(dis)
+            centerLst[index].add(dataSet[lines, :], lines)
+        else:
+            centerLst.append(Center(dataSet[lines, :], lines))
+    return centerLst
+
+if __name__ == '__main__':
+    result = process()
+    print "result"
+    for item in result:
+        print item.meanValue
+
